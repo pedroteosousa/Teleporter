b29f29752b7706e1b0ac54d84f2992691a44acec
Pedro Bortolli <pedro.vitor.santos@usp.br>
2018-04-05 17:31:43 -0300

    added 'Exit' to the menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -8,11 +8,13 @@ func _ready():
 
 func on_pressed(button_name):
 	name = button_name.get_name()
-	if name == "New Game":
+	if name == "New game":
 		print("click on New game")
 		get_tree().change_scene("res://scenes/Universe.tscn")
 	elif name == "Continue":
 		print("click on Continue")
 	elif name == "Options":
 		print("click on Options")
+	elif name == "Exit":
+		print("click on Exit")
 		
