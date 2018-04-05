c3c1f9584cec83e6cba79cb295dad16080ff7a67
Pedro Sousa <pedroteosousa@gmail.com>
2018-04-05 15:52:49 -0300

    added level maker

--- scripts/universe.gd
+++ scripts/universe.gd
@@ -1,4 +1,16 @@
 extends Node
 
+var basePath = "res://scenes/"
+
 func _ready():
-	pass
+	var map = get_node("Map")
+	var tileset = map.tile_set
+	for cell in map.get_used_cells():
+		var tile = map.get_cell(cell.x, cell.y)
+		var name = tileset.tile_get_name(tile)
+		var path = basePath + name + ".tscn"
+		var pos = cell*32
+		var object = load(path).instance()
+		object.set_position(pos)
+		add_child(object)
+	map.hide()
