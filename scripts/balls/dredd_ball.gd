f8841bd09ed73ee825dd8d142e6191e899cdf7e4
pedroteosousa <pedroteosousa@gmail.com>
2018-04-09 18:28:54 -0300

    A few changes to Dredd Ball - Dredd Ball now has a sprite and rotates according to its direction - Fixed bug where Dredd Ball would change direction on key release - Now always being released parallel to axis - Now ball moves with WASD in addition to arrows

--- scripts/balls/dredd_ball.gd
+++ scripts/balls/dredd_ball.gd
@@ -7,16 +7,25 @@ func ball_timeout():
 	print(elapsed_time)
 	if (elapsed_time >= destroy_time):
 		queue_free()
+		
+func go(dir, vel):
+	velocity = limit_velocity(dir) + vel
+	if abs(dir.x) > abs(dir.y):
+		dir.y = 0
+	else:
+		dir.x = 0
+	change_direction(dir.normalized())
+	
+func change_direction(dir):
+	self.rotation = dir.angle()
+	velocity = dir*velocity.length()
 
 func _unhandled_input(event):
-	if InputMap.event_is_action(event, "ui_down"):
-		velocity = Vector2(0,1)*velocity.length()
-	if InputMap.event_is_action(event, "ui_left"):
-		velocity = Vector2(-1,0)*velocity.length()
-	if InputMap.event_is_action(event, "ui_right"):
-		velocity = Vector2(1,0)*velocity.length()
-	if InputMap.event_is_action(event, "ui_up"):
-		velocity = Vector2(0,-1)*velocity.length()
-
-func _ready():
-	pass
+	if InputMap.event_is_action(event, "ui_down") and event.is_pressed():
+		change_direction(Vector2(0,1))
+	if InputMap.event_is_action(event, "ui_left") and event.is_pressed():
+		change_direction(Vector2(-1,0))
+	if InputMap.event_is_action(event, "ui_right") and event.is_pressed():
+		change_direction(Vector2(1,0))
+	if InputMap.event_is_action(event, "ui_up") and event.is_pressed():
+		change_direction(Vector2(0,-1))
