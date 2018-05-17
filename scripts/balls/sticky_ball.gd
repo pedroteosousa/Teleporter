9e262be7c342de8507460d0cd1642ce513a68c9a
Pedro Sousa <pedroteosousa@gmail.com>
2018-05-17 11:52:17 -0300

    Fixing sticky ball (Resolves #8)

--- scripts/balls/sticky_ball.gd
+++ scripts/balls/sticky_ball.gd
@@ -3,3 +3,14 @@ extends "res://scripts/balls/simple_ball.gd"
 # stop on first collision
 func collided(collision):
 	set_physics_process(false)
+	velocity = Vector2(0,0)
+	gravity_velocity = Vector2(0,0)
+	
+func _process(delta):
+	var area = get_node('Area2D')
+	var num_collisions = len(area.get_overlapping_bodies()) + len(area.get_overlapping_areas())
+	print(num_collisions)
+	if (num_collisions == 0):
+		set_physics_process(true)
+	else:
+		collided(null)
