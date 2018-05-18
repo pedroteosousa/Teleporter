1f277b60f0333f4ad33d629e1fdf57aa6444bb34
Professor Lucas <thiagestrela@gmail.com>
2018-05-18 20:43:19 +0000

    added the scene Portal that implements the Portal Game mechanics

---
+++ scripts/Portal.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+export (int) var id
+export (Vector2) var normal = Vector2(0,1)
+var ignore = false
+
+func _ready():
+	connect("body_entered", self, "teleport")
+	
+func teleport(body):
+	if body.collision_layer == 2:
+		if ignore:
+			ignore = false
+			return
+		for portal in get_parent().get_children():
+			if portal.id == id and portal.get_instance_id() != self.get_instance_id():
+				portal.ignore = true
+				body.position = portal.position
+				body.velocity = body.velocity.bounce(normal)
+				break
