774014fff81c6b735a5ec598980d6c7b1eea7a8b
Pedro Sousa <pedroteosousa@gmail.com>
2018-05-25 19:21:21 -0300

    Fixing Sticky Ball behaviour (Fixes #13)

--- scripts/balls/sticky_ball.gd
+++ scripts/balls/sticky_ball.gd
@@ -1,16 +1,31 @@
 extends "res://scripts/balls/simple_ball.gd"
 
+var isStationary = false
+onready var area = get_node('Area2D')
+
 # stop on first collision
 func collided(collision):
+	# make ball go inside the collided object a bit, to make sure area detects intersection
+	position -= collision.normal
+	
 	set_physics_process(false)
 	velocity = Vector2(0,0)
 	gravity_velocity = Vector2(0,0)
 	
+	# setting area to identify collisions
+	isStationary = true
+	
+# making own movement to ignore reflect
+func movement(delta):
+	var collision = move_and_collide(get_movement_pattern()*delta)
+	if collision:
+		collided(collision)
+
 func _process(delta):
-	var area = get_node('Area2D')
-	var num_collisions = len(area.get_overlapping_bodies()) + len(area.get_overlapping_areas())
-	print(num_collisions)
-	if (num_collisions == 0):
-		set_physics_process(true)
-	else:
-		collided(null)
+	if isStationary:
+		var num_collisions = len(area.get_overlapping_bodies()) + len(area.get_overlapping_areas())
+		if num_collisions == 0:
+			set_physics_process(true)
+			
+			# setting area to not identify collisions
+			isStationary = false
