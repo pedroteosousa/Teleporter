9aa35d0c31ae51b103e1cac5e4561c07dda300d8
Pedro Bortolli <pedro.vitor.santos@usp.br>
2018-06-28 20:16:29 -0300

    Finished order of levels

--- scripts/levels/level_test_14.gd
+++ scripts/levels/level_test_14.gd
@@ -1,5 +1,5 @@
 extends "res://scripts/levels/simple_level.gd"
 
 func init():
-	balls = [["PoolBall", 2], ["BalloonBall", 1], ["CrazyBall", 1]]
+	balls = [["DreddBall", 1], ["PoolBall", 1], ["BalloonBall", 1], ["CrazyBall", 1]]
 	pass
